b3d01e1f11641e1c58bb21b65c353cda711e4dd8
Nirvana <335957142@qq.com>
2022-03-22 09:31:07 +0800

    上传密码学和计网的资料

---
+++ 计算机网络（张建忠）/ProjectHomework/1/SocketChatroom/main.cpp
@@ -0,0 +1,18 @@
+#include"client.h"
+#include"server.h"
+#include <iostream>
+
+int main() {
+	cout << "Please select which you want to be:\n0. Server\t1. Client\n";
+	int identification;
+	cin >> identification;
+	cin.get();
+	if (identification == 0)
+		serverMain();
+	else
+		clientMain();
+
+
+	return 0;
+}
+
---
+++ 计算机网络（张建忠）/ProjectHomework/3_2/client/main.cpp
@@ -0,0 +1,15 @@
+#include"client.h"
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	cout << "/***************************/\n";
+	cout << "/******\t客户端3.2\t************/\n";
+	cout << "/***************************/\n";
+	
+	//connet2Server();
+	// 启动客户端
+	sendMain();
+	return 0;
+}
